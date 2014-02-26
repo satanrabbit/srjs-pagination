1d70a948eb2961330412894a0be1453034bea27a
夏千祥 <satanrabbit@xiaqianxiangdeMacBook-Pro.local>
2014-02-26 22:24:41 +0800

    修改为jquery插件模式，arale实在受不了了

--- examples/index.md
+++ examples/index.md
@@ -5,78 +5,59 @@
 ## Normal usage
 
 ````html
-<div id="pager1"></div>
-<div id="pager2"></div>
-<div id="pager4"></div>
-<div id="pager5"></div>
-<div id="pager6"></div>
-<div id="pager7"></div>
-<div id="pager8"></div>
-<div id="pager9"></div>
-<div id="pager10"></div>
-<div id="pager11"></div>
-<div id="pager12"></div>
-<div id="pager13"></div>
-<div id="pager14"></div>
-<div id="pager15"></div>
-<div id="pager16"></div>
-<div id="pager17"></div>
-<div id="pager18"></div>
-<div id="pager19"></div>
-<div id="pager20"></div>
-<div id="pager3"></div>
-<div id="pager21"></div>
+<div class="pager1"></div>
+<div class="pager2"></div>
+<div class="pager3"></div>
+<div class="pager4"></div>
+<div class="pager5"></div>
+<div class="pager6"></div>
+<div class="pager7"></div>
+<div class="pager8"></div>
+<div class="pager9"></div>
+<div class="pager10"></div>
+<div class="pager11"></div>
+<div class="pager12"></div>
+<div class="pager13"></div>
+<div class="pager14"></div>
+<div class="pager15"></div>
+<div class="pager16"></div>
+<div class="pager17"></div>
+<div class="pager18"></div>
+<div class="pager19"></div>
+<div class="pager20"></div>
+<div class="pager21"></div>
+<div class="pager22"></div>
+<div class="pager23"></div>
 ````
 
 使用：
 
 ````javascript
-seajs.use('pagination', function(pagination) {
+seajs.use(['$','pagination'], function($,pagination) {
 
-  var pager1=new pagination({element: '#pager1',currentPage:1,totalPage:20,url:"./",isSmall:true,onClickPager:function(p){console.log(p);}});
-  pager1.render();
-   var pager2=new pagination({element: '#pager2',currentPage:2,totalPage:20,url:"./"});
-  pager2.on("onClickPager",function(p){
-  alert(p);
-}).render();
-   var pager4=new pagination({element: '#pager4',currentPage:3,totalPage:20,url:"./",isSmall:true,isSpace:false});
-  pager4.render();
-   var pager5=new pagination({element: '#pager5',currentPage:4,totalPage:20,url:"./"});
-  pager5.render();
-   var pager6=new pagination({element: '#pager6',currentPage:5,totalPage:20,url:"./",isSmall:true,isSpace:false});
-  pager6.render();
-   var pager7=new pagination({element: '#pager7',currentPage:6,totalPage:20,url:"./",align:'right'});
-  pager7.render();
-   var pager8=new pagination({element: '#pager8',currentPage:7,totalPage:20,url:"./",isSmall:true,align:'left'});
-  pager8.render();
-   var pager9=new pagination({element: '#pager9',currentPage:8,totalPage:20,url:"./"});
-  pager9.render();
-   var pager10=new pagination({element: '#pager10',currentPage:9,totalPage:20,url:"./"});
-  pager10.render();
-   var pager11=new pagination({element: '#pager11',currentPage:10,totalPage:20,url:"./"});
-  pager11.render();
-   var pager12=new pagination({element: '#pager12',currentPage:11,totalPage:20,url:"./"});
-  pager12.render();
-   var pager13=new pagination({element: '#pager13',currentPage:12,totalPage:20,url:"./"});
-  pager13.render();
-   var pager14=new pagination({element: '#pager14',currentPage:13,totalPage:20,url:"./"});
-  pager14.render();
-   var pager15=new pagination({element: '#pager15',currentPage:14,totalPage:20,url:"./"});
-  pager15.render();
-   var pager16=new pagination({element: '#pager16',currentPage:15,totalPage:20,url:"./"});
-  pager16.render();
-   var pager17=new pagination({element: '#pager17',currentPage:16,totalPage:20,url:"./"});
-  pager17.render();
-   var pager18=new pagination({element: '#pager18',currentPage:17,totalPage:20,url:"./"});
-  pager18.render();
-   var pager19=new pagination({element: '#pager19',currentPage:18,totalPage:20,url:"./"});
-  pager19.render();
-   var pager20=new pagination({element: '#pager20',currentPage:19,totalPage:20,url:"./"});
-  pager20.render();
-   var pager3=new pagination({element: '#pager3',currentPage:20,totalPage:20,url:"./"});
-  pager3.render();
-   var pager21=new pagination({element: '#pager21',currentPage:21,totalPage:21,url:"./"});
-  pager21.render();
+  $(".pager1").pagination({currentPage:1,totalPage:20});
+  $(".pager2").pagination({currentPage:2,totalPage:20});
+  $(".pager3").pagination({currentPage:3,totalPage:20});
+  $(".pager4").pagination({currentPage:4,totalPage:20});
+  $(".pager5").pagination({currentPage:5,totalPage:20});
+  $(".pager6").pagination({currentPage:6,totalPage:20});
+  $(".pager7").pagination({currentPage:7,totalPage:20});
+  $(".pager8").pagination({currentPage:8,totalPage:20});
+  $(".pager9").pagination({currentPage:9,totalPage:20});
+  $(".pager10").pagination({currentPage:10,totalPage:20});
+  $(".pager11").pagination({currentPage:11,totalPage:20});
+  $(".pager12").pagination({currentPage:12,totalPage:20});
+  $(".pager13").pagination({currentPage:13,totalPage:20});
+  $(".pager14").pagination({currentPage:14,totalPage:20});
+  $(".pager15").pagination({currentPage:15,totalPage:20});
+  $(".pager16").pagination({currentPage:16,totalPage:20});
+  $(".pager17").pagination({currentPage:17,totalPage:20});
+  $(".pager18").pagination({currentPage:18,totalPage:20});
+  $(".pager19").pagination({currentPage:19,totalPage:20});
+  $(".pager20").pagination({currentPage:20,totalPage:20});
+  $(".pager21").pagination({currentPage:21,totalPage:23});
+  $(".pager22").pagination({currentPage:22,totalPage:23});
+  $(".pager23").pagination({currentPage:23,totalPage:23});
 
 });
 ````
--- src/pagination.js
+++ src/pagination.js
@@ -1,113 +1,145 @@
 define(function(require, exports, module) {
 
-  Widget  = require('widget') ;
-  require('./pagination.css');
-  var pagination =Widget.extend({
-    attrs:{
-      currentPage:1,
-      totalPage:1,
-      url:"./",
-      isSmall:false,
-      isSpace:true,
-      align:"center"
-    },
-    setup: function() {
-      var currentPage=this.get("currentPage");
-      var totalPage=this.get("totalPage");
-      var url=this.get("url");
-      this.element.append('<a href="javascript:void(0);" data-page="'+(currentPage-1)+'" class="first pageprv"><span class="pagearr">&lt;</span>上一页</a>'+
-        '<a href="javascript:void(0);"  data-page="'+(currentPage+1)+'" class="last pagenxt">下一页<span class="pagearr">&gt;</span></a>');
-      this.element.addClass('m-page');
-      if (this.get('isSmall')) {
-        this.element.addClass('m-page-sm');
-      };
-      if (this.get('isSpace')) {
-        this.element.addClass('m-page-sr');
-      };
-      if (currentPage==1) {
-        this.$('.pageprv').addClass('z-dis').data('page', '1');
-      };
-      switch(this.get("align")) {
-        case "left":
-          this.element.addClass('m-page-lt');
-          break;
-        case "right":
-          this.element.addClass('m-page-rt');
-          break;
-        default:
-          break;
-      };
-
-      if (currentPage>=totalPage) {
-        //alert(currentPage);
-        this.$('.pagenxt').addClass('z-dis').data('page', currentPage);
-      };
-      if (totalPage<9) {
-        for (var i = 1; i <= totalPage; i++) {        
-          if (i==currentPage) {
-            this.$(".pagenxt").before('<a href="javascript:void(0);" data-page="'+i+'" class="z-crt">'+i+'</a>');
-          }else{
-            this.$(".pagenxt").before('<a href="javascript:void(0);" data-page="'+i+'" >'+i+'</a>') ;
-          }
+  var $ = require('$');
+  require("./pagination.css");
+    // 插件的定义    
+  $.fn.pagination = function() {
+    var method = arguments[0]; 
+    if(methods[method]) {
+        method = methods[method];
+        // 我们的方法是作为参数传入的，把它从参数列表中删除，因为调用方法时并不需要它
+        arguments = Array.prototype.slice.call(arguments, 1);
+    } else if( typeof(method) == 'object' || !method ) {
+        method = methods.init;
+    } else {
+        $.error( 'Method ' +  method + ' does not exist on jQuery.pluginName' );
+        return this;
+    }
+    // 用apply方法来调用我们的方法并传入参数
+    return method.apply(this, arguments);
+  };
+  var methods = {
+    init :  function (options){
+      // 在每个元素上执行方法
+      return this.each(function() {
+        var context = $(this);
+        // 使用extend方法从options和defaults对象中构造出一个settings对象
+        var opts = $.extend({}, $.fn.pagination.defaults, options);
+        // 执行代码
+        context.removeClass('m-page-sm').removeClass('m-page-sr').removeClass('m-page-lt').removeClass('m-page-rt').empty().append('<a href="javascript:void(0);" data-page="'+(opts.currentPage-1)+'" class="first pageprv"><span class="pagearr">&lt;</span>上一页</a>'+
+            '<a href="javascript:void(0);"  data-page="'+(opts.currentPage+1)+'" class="last pagenxt">下一页<span class="pagearr">&gt;</span></a>');
+        context.addClass('m-page');
+        if (opts.isSmall){
+          context.addClass('m-page-sm');
         };
-      }else{
-        
-        //第一页
-        if (currentPage==1) {
-          this.$(".pagenxt").before('<a href="javascript:void(0);"  data-page="1" class="z-crt">1</a>');
-        }else{
-          this.$(".pagenxt").before('<a href="javascript:void(0);"  data-page="1" >1</a>');
-        }
-        /*中间页部分*/
-        if (currentPage-3<=2) {
-          for (var i = 2; i < 7; i++) {
-            if (currentPage==i) {
-              this.$(".pagenxt").before('<a href="javascript:void(0);"  data-page="'+i+'" class="z-crt">'+i+'</a>');
+        if (opts.isSpace) {
+          context.addClass('m-page-sr');
+        };
+        if (opts.currentPage==1) {
+          context.find('.pageprv').addClass('z-dis').data('page', '1');
+        };
+        switch(opts.align){
+          case "left":
+            context.addClass('m-page-lt');
+            break;
+          case "right":
+            context.addClass('m-page-rt');
+            break;
+          default:
+            break;
+        };
+
+        if (opts.currentPage>=opts.totalPage) {
+          //alert(opts.currentPage);
+          context.find('.pagenxt').addClass('z-dis').data('page', opts.currentPage);
+        };
+        if (opts.totalPage<9) {
+          for (var i = 1; i <= opts.totalPage; i++) {        
+            if (i==opts.currentPage) {
+              context.find(".pagenxt").before('<a href="javascript:void(0);" data-page="'+i+'" class="z-crt">'+i+'</a>');
             }else{
-              this.$(".pagenxt").before('<a href="javascript:void(0);"  data-page="'+i+'">'+i+'</a>');
+              context.find(".pagenxt").before('<a href="javascript:void(0);" data-page="'+i+'" >'+i+'</a>') ;
             }
           };
-          this.$(".pagenxt").before(' <i>...</i>');
-        }else {
-          this.$(".pagenxt").before(' <i>...</i>');
-          if (currentPage+3>=totalPage-1) {
-            for (var i = totalPage-6; i < totalPage; i++) {
-              if (currentPage==i) {
-                this.$(".pagenxt").before('<a  href="javascript:void(0);" data-page="'+i+'" class="z-crt">'+i+'</a>');
-              }else{
-                this.$(".pagenxt").before('<a  href="javascript:void(0);"  data-page="'+i+'">'+i+'</a>');
-              }
-            };
-          }else{
-            for (var i = currentPage-3; i < currentPage+3; i++) {
-              if (currentPage==i) {
-                this.$(".pagenxt").before('<a  href="javascript:void(0);"  data-page="'+i+'" class="z-crt">'+i+'</a>');
-              }else{
-                this.$(".pagenxt").before('<a  href="javascript:void(0);"  data-page="'+i+'">'+i+'</a>');
-              }
-            };
-            this.$(".pagenxt").before(' <i>...</i>');
-          }
-        };
-        //最后一页
-        if (currentPage>=totalPage) {
-          this.$(".pagenxt").before('<a  href="javascript:void(0);"  data-page="'+totalPage+'" class="z-crt">'+totalPage+'</a>');
         }else{
-            this.$(".pagenxt").before('<a  href="javascript:void(0);"  data-page="'+totalPage+'" >'+totalPage+'</a>') ;
+          
+          //第一页
+          if (opts.currentPage==1) {
+            context.find(".pagenxt").before('<a href="javascript:void(0);"  data-page="1" class="z-crt">1</a>');
+          }else{
+            context.find(".pagenxt").before('<a href="javascript:void(0);"  data-page="1" >1</a>');
+          }
+          /*中间页部分*/
+          if (opts.currentPage-3<=2) {
+            for (var i = 2; i < 7; i++) {
+              if (opts.currentPage==i) {
+                context.find(".pagenxt").before('<a href="javascript:void(0);"  data-page="'+i+'" class="z-crt">'+i+'</a>');
+              }else{
+                context.find(".pagenxt").before('<a href="javascript:void(0);"  data-page="'+i+'">'+i+'</a>');
+              }
+            };
+            context.find(".pagenxt").before(' <i>...</i>');
+          }else {
+            context.find(".pagenxt").before(' <i>...</i>');
+            if (opts.currentPage+3>=opts.totalPage-1) {
+              for (var i = opts.totalPage-6; i < opts.totalPage; i++) {
+                if (opts.currentPage==i) {
+                  context.find(".pagenxt").before('<a  href="javascript:void(0);" data-page="'+i+'" class="z-crt">'+i+'</a>');
+                }else{
+                  context.find(".pagenxt").before('<a  href="javascript:void(0);"  data-page="'+i+'">'+i+'</a>');
+                }
+              };
+            }else{
+              for (var i = opts.currentPage-3; i < opts.currentPage+3; i++) {
+                if (opts.currentPage==i) {
+                  context.find(".pagenxt").before('<a  href="javascript:void(0);"  data-page="'+i+'" class="z-crt">'+i+'</a>');
+                }else{
+                  context.find(".pagenxt").before('<a  href="javascript:void(0);"  data-page="'+i+'">'+i+'</a>');
+                }
+              };
+              context.find(".pagenxt").before(' <i>...</i>');
+            }
+          };
+          //最后一页
+          if (opts.currentPage>=opts.totalPage) {
+            context.find(".pagenxt").before('<a  href="javascript:void(0);"  data-page="'+opts.totalPage+'" class="z-crt">'+opts.totalPage+'</a>');
+          }else{
+              context.find(".pagenxt").before('<a  href="javascript:void(0);"  data-page="'+opts.totalPage+'" >'+opts.totalPage+'</a>') ;
+          }
         }
-      }
-      this.on('onClickPager', function(p) {
-        window.location.href=url+p;
-      });
-      //设置分页点击事件
-      var pagerSelf=this;
-      this.$("a").on('click',function(event) {
-        var clickedPage=pagerSelf.$(this).data("page");
-        //pagerSelf.trigger("onClickPager",clickedPage);
-      });      
-    }
-  });
 
-  module.exports = pagination;
+        context.find('a').click(function(event) {
+          var p=$(this).data("page");
+          if (opts.onClickPager ){
+            opts.onClickPager(p);
+          }else{
+            window.location.href=opts.url+p;
+          }
+        });
+      });      
+    }/*,
+    show :  function(options) {
+      // 对选择器每个元素都执行方法
+      return this.each(function() {
+          // 执行代码
+      });
+    },
+    destroy: function() {
+      // 对选择器每个元素都执行方法
+      return this.each(function() {
+          // 执行代码
+      });
+    }*/
+  };
+ 
+  // 插件的defaults    
+  $.fn.pagination.defaults = {    
+    currentPage:7,
+    totalPage:18,
+    url:"./",
+    isSmall:false,
+    isSpace:true,
+    align:"center" 
+  };    
 
 });
